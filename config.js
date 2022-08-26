738c29c459eef106c4abd63eb844a64f6d83a333
shadow-monarch <invicibleshadowmonarch@gmail.com>
2022-08-26 18:51:07 +0530

    heroku deploy test

--- config.js
+++ config.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-mongoose.connect(process.env.URL, { useNewUrlParser: true });
+mongoose.connect(process.env.URL, {
+  useNewUrlParser: true,
+  server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+  replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+});
 const db = mongoose.connection;
 
 db.on("error", (error) => console.log(error));
